a8099621e481e9c3d38e408817945d127e1f66ff
Kunal Bamborde <95536083+Kbamborde@users.noreply.github.com>
2022-02-11 20:17:36 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,81 @@
+const randomQuoteApiUrl = 'https://api.quotable.io/random';
+const quoteDisplayElement = document.querySelector("#quote-display");
+const quoteInputElement = document.querySelector("#quoteInput");
+const timer = document.querySelector("#timer");
+
+let gameActive = false;
+let count = 0;
+quoteInputElement.addEventListener("input", () => {
+    gameActive = true;    
+    if(gameActive && count===0){
+        count++
+        startTimer();
+    }
+
+ 
+    const arrayQuote = quoteDisplayElement.querySelectorAll("span");
+    const arrayValue = quoteInputElement.value.split("");
+    
+    let correct = true;
+    
+    for (let i = 0; i < arrayQuote.length; i++) {
+        if (arrayValue[i] == null) {
+            arrayQuote[i].classList.remove("correct");
+            arrayQuote[i].classList.remove("incorrect");
+            correct = false;
+        }
+        
+        else if (arrayValue[i] === arrayQuote[i].textContent) {
+            arrayQuote[i].classList.add("correct");
+            arrayQuote[i].classList.remove("incorrect");
+            correct = true;
+        }
+        
+        else {
+            arrayQuote[i].classList.add("incorrect");
+            arrayQuote[i].classList.remove("correct");
+            correct = false;
+        }
+    }
+
+    if (correct){
+        count = 0
+        renderNewQuote();
+    }
+})
+
+
+
+function getRandomQuote() {
+    return fetch(randomQuoteApiUrl)
+        .then(response => response.json())
+        .then(data => data.content)
+};
+
+async function renderNewQuote() {
+    const quote = await getRandomQuote();
+    quoteDisplayElement.innerText = '';
+    quote.split("").forEach(character => {
+        const characterSpan = document.createElement("span");
+        characterSpan.innerText = character;
+        quoteDisplayElement.appendChild(characterSpan);
+    })
+
+    quoteInputElement.value = null;
+}
+
+
+let startTime
+function startTimer() {
+    timer.innerText = 0;
+    startTime = new Date()
+    setInterval(()=>{
+        timer.innerText = getTimerTime();
+    },1000);
+}
+
+function getTimerTime() {
+  return  Math.floor((new Date() - startTime) / 1000);
+}
+renderNewQuote();
+
